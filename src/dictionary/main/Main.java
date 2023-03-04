00c03208dfbbd76eb7f60f151debf85ed9cfb8c5
Mehman Osmanov <mehmanosmanof@gmail.com>
2023-03-04 17:06:34 +0400

    First Version 1.0.1

--- src/dictionary/main/Main.java
+++ src/dictionary/main/Main.java
@@ -2,14 +2,6 @@ package dictionary.main;
 
 public class Main {
     public static void main(String[] args) {
-//        Map<String, String> dictionary = new TreeMap<>();
-//        dictionary.put("hello", "salam");
-//        dictionary.put("hi", "salam");
-//        dictionary.put("bye", "sagol");
-//        dictionary.put("goodbye", "sagol");
-//
-//        System.out.println( dictionary.keySet());
-//        System.out.println(dictionary);
         try {
             DictionaryApp.chooseDictionaryType();
         }catch (RuntimeException ex){
